04f6d04c91aea3183299e44d0ac1f35acb45e0b4
Aays <2790751195@qq.com>
2020-11-26 10:49:49 +0800

    第三次提交

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
 console.log('第一次Git');
 // git status,查看状态
-// git diff，查看变化详情
+// git diff,查看变化详情
 console.log('第二次Git');
+// git log,查看历史
+// git log --pretty=oneline,简洁查看历史版本
+// git reset --hard HEAD^,回退到历史版本，几个小三角就回退几个版本
+console.log('第三次Git')
