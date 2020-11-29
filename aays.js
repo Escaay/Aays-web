95e2bb5e111a1c72c2fed2a94a158cd9ff9530d3
Escaay <2790751195@qq.com>
2020-11-29 08:29:37 +0800

    更新了aays.js

--- aays.js
+++ aays.js
@@ -1,1 +1,13 @@
-哈哈啊哈哈
+// 网上说需要删除或者重新命名已经存在的本地文件，然后在clone 其实不用。
+
+// 第一步 ：进入到已存在的目录  cd (/file)
+
+// 第二步： 初始化git  init
+
+// 第三步： 添加远程仓库地址  git remote add origin  (address)
+
+// 第四步：  添加本地代码  git add .
+
+// 第五步： 提交本地代码 git commit -m "init"
+
+// 第六步： 提交到远程仓库 git push origin master
