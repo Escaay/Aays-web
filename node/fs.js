3e05575d7ecf96969a756d3e32c3cf6604912427
Escaay <2790751195@qq.com>
2020-11-30 07:26:36 +0800

    更新了fs.js

---
+++ node/fs.js
@@ -0,0 +1,23 @@
+// 异步写入文件
+const fs = require('fs')
+    // 在nodejs中有错误优先的思想，所以每个回调函数第一个参数错误函数（不一定命名为err）错误时执行它
+fs.open('../hello1.txt', 'w', (err, fd) => {
+        // open中第二个参数fd就是打开的文件的唯一标识
+        !err ? (console.log('文件打开成功')) : console.log('出错了');
+        fs.write(fd, '这是异步写入的内容', (err) => {
+            if (!err) {
+                console.log('文件写入完成');
+            }
+            fs.close(fd, (err) => {
+                // 每一次回调都要判断是否有错误，错误优先
+                if (!err) {
+                    console.log('文件关闭完成');
+                } else {
+                    console.log(err);
+                }
+            })
+        })
+    })
+    // 上面不能用一个const fd=fs.open ,因为它没有返回值，有返回值的一定是同步的操作
+    // 异步操作会放到线程池里面等下面的代码都执行完了才执行
+console.log('在下面的代码却会先执行，这是因为nodejs是异步的，不会阻塞程序');
--- node/fsSync.js
+++ node/fsSync.js
@@ -6,3 +6,4 @@ const fd = fs.openSync('../hello.txt', 'w')
     // console.log(fd);输出3,是内存位置的意思,用这个来选取操作的文件
     //在hello.txt中写入内容(同步),第三个参数是写入的起始位置,每一次写入都会清空文件的原有内容再写入
 fs.writeSync(fd, '这是写入hello.txt的内容', 20)
+fs.close(fd)
